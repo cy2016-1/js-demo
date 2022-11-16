be8bad9a9ab285c3f220c69e491451f1d2fb838a
zhuolei <245094530@qq.com>
2022-11-16 12:32:49 +0800

    更新pdf预览

---
+++ js/29.js
@@ -0,0 +1,13 @@
+import {
+	getName,
+	getSkill,
+	getMaterial,
+	getTalisman,
+	getCreature,
+	getAlchemy,
+	getClan,
+	getLocation,
+	getNation,
+	getZone
+} from 'random_chinese_fantasy_names'
+console.log(getName)
